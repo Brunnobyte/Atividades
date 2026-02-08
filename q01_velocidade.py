3da374fc07ed0df4178628db6236aed783a7eb3b
Brunnobyte <brunno172004@gmail.com>
2026-02-08 20:56:27 -0300

    Add files via upload

---
+++ q01_velocidade.py
@@ -0,0 +1,9 @@
+# Entrada
+velocidade_ms = float(input('velocidade m/s'))
+
+# Processamento
+velocidade_kmh = velocidade_ms * 3.6
+
+# Saída
+print(f'> A velocidade de {velocidade_ms} m/s equivale a {velocidade_kmh} km/h')
+
