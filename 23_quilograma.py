c624788d91e85467d62f126a725036cf8e1d7c02
Brunnobyte <brunno172004@gmail.com>
2026-02-12 13:26:07 -0300

    Add files via upload

---
+++ 23_quilograma.py
@@ -0,0 +1,7 @@
+#entrada
+Quilos = int(input("digite um valor em quilograma:"))
+
+#processamento
+gramas = Quilos * 1000
+#saida
+print("digite o valor de Quilos em grama:", gramas)
---
+++ q24_MetrosEmCm.py
@@ -0,0 +1,7 @@
+#entrada
+metros = int(input("digite um valor em metro:"))
+
+#processamento
+cm = metros * 100
+#saida
+print("digite o valor de metros em cm:", cm)
